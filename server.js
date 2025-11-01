10a0f7a92d0a81c4eacb912d2222f1873b8ed5a8
DigitalFlyHighSolutions <digitalflyhighsolutions@gmail.com>
2025-11-01 18:02:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5001;
 
 // --- CORS Configuration ---
 const allowedOrigins = [
-  'https://myindustryhouse.com',
+  'https://myindustryhouse.com', "https://www.myindustryhouse.com"
   // For local testing, uncomment:
   // 'http://localhost:3000'
 ];
